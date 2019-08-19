37d02cdf9496199eeb91a7b73ea2a803a924bf24
sebastien.jean <sebastien.jean@intech.lu>
2019-08-19 12:21:52 +0200

    initial commit

---
+++ src/main/java/com/sebastien/jean/pathfinderapi/PathfinderapiApplication.java
@@ -0,0 +1,13 @@
+package com.sebastien.jean.pathfinderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathfinderapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PathfinderapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sebastien/jean/pathfinderapi/PathfinderapiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sebastien.jean.pathfinderapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PathfinderapiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
